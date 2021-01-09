7ac9e03a294c67162c53dacf47239541b23ac505
OneGameKoTT <i.misterok21@gmail.com>
2021-01-09 04:59:32 +0200

    user rights

--- dockerinit.sh
+++ dockerinit.sh
@@ -1,6 +1,11 @@
 #!/bin/sh\
 
-docker run --rm -i -v "$(pwd):/home/builder/twrp/:rw,z" -v "${HOME}/.ccache:/srv/ccache:rw,z" fr3akyphantom/droid-builder bash << EOF
+docker run --privileged --rm -i \
+  -e USER_ID=$(id -u) -e GROUP_ID=$(id -g) \
+  -v "$(pwd):/home/builder/twrp:rw,z" \
+  -v "$HOME/.ccache:/srv/ccache:rw" \
+  fr3akyphantom/droid-builder:${TAG} \
+  /bin/bash << EOF
     cd /home/builder/twrp/
     ls -la ./
     source build/envsetup.sh
