8f90f073413388c75298d19b5e9058c6ad9e375c
OneGameKoTT <i.misterok21@gmail.com>
2021-01-09 05:36:54 +0200

    user rights #4

--- dockerinit.sh
+++ dockerinit.sh
@@ -2,11 +2,11 @@
 
 docker run --privileged --rm -i \
   -e USER_ID=$(id -u) -e GROUP_ID=$(id -g) \
-  -v "$(pwd):/home/builder/twrp:rw,z" \
+  -v "$HOME:/home/builder:rw,z" \
   -v "$HOME/.ccache:/srv/ccache:rw" \
   fr3akyphantom/droid-builder \
   /bin/bash << EOF
-    cd /home/builder/twrp/
+    cd $HOME/twrp/
     ls -la ./
     source build/envsetup.sh
     lunch omni_C18_Pro-eng
