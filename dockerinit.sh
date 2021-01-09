2616f3b2cae40f60ce098fd16bc967efc687bc7c
OneGameKoTT <i.misterok21@gmail.com>
2021-01-09 14:54:50 +0200

    maybe it works #2

--- dockerinit.sh
+++ dockerinit.sh
@@ -1,13 +1,15 @@
 #!/bin/sh\
 
+cd $HOME/twrp
 docker run --privileged --rm -i \
   -e USER_ID=$(id -u) -e GROUP_ID=$(id -g) \
-  -v "$HOME/twrp/:/home/builder/twrp:rw,z" \
+  -v "$HOME/twrp:/home/builder/twrp:rw,z" \
   -v "$HOME/.ccache:/srv/ccache:rw" \
   fr3akyphantom/droid-builder \
   /bin/bash << EOF
+    cd $HOME/twrp
     mkdir out
-    . build/envsetup.sh
+    source build/envsetup.sh
     lunch omni_C18_Pro-eng
     make -j$(nproc --all) recoveryimage
     exit
